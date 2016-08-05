17ca02f29c6a614c5c1f681aef226adb9c02f6e5
Kathryn Hodge <kahodge@vassar.edu>
2016-08-05 12:40:12 -0400

    Adding JS

---
+++ JavaScript in 7/js/script.js
@@ -0,0 +1,13 @@
+function revealMessage() {
+	document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+function countDown() {
+	var currentVal = document.getElementById("countDownButton").innerHTML;
+	var newVal = 0;
+	if (currentVal > 0) {
+		newVal = currentVal - 1;
+	}
+
+	document.getElementById("countDownButton").innerHTML = newVal;
+}
