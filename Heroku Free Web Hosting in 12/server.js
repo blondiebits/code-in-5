093f3142f81e5b1d09dc5e3887cdd68745bef3c5
Kathryn Hodge <kahodge@vassar.edu>
2016-09-23 12:03:55 -0400

    Adding Heroku Code

---
+++ Heroku Free Web Hosting in 12/server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('index');
+})
+
+app.listen(port, function() {
+	console.log('app running')
+})
